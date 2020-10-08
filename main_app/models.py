15394c4420a25da8d807ae2342f186e5c3691a1d
wkriegsman <wkriegsman@gmail.com>
2020-10-08 13:16:17 -0400

    creates image, post, and city models

--- main_app/models.py
+++ main_app/models.py
@@ -3,28 +3,41 @@ from django.contrib.auth.models import User
 
 # # Create your models here.
 
-class User_Profile(models.Model):
-    user_name = models.CharField(max_length=150)
-    user_city = models.CharField(max_length=150)
-    join_date = models.DateField('Join Date')
-    post_title = models.CharField(max_length=200)
-    post_city = models.CharField(max_length=75)
-    post = models.TextField(max_length=600)
 
 
 class Image(models.Model):
-    name= models.CharField(max_length=500)
-    videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
+    imagefile= models.FileField(upload_to='images/', null=True, verbose_name="")
 
     def __str__(self):
-        return self.name + ": " + str(self.imagefile)
+        return self.imagefile
+        # in case the above doesn't work this was in the tutorial 
+        # str(self.imagefile)
+
 
 
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
+    post_city = models.CharField(max_length=75)
     post_content = models.TextField(max_length=600)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+def __str__(self):
+        return self.user_name
+
+class City(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+def __str__(self):
+        return self.user_name
 
 
+class User_Profile(models.Model):
+    user_name = models.CharField(max_length=150)
+    user_city = models.CharField(max_length=150)
+    join_date = models.DateField('Join Date')
+    post = models.ManyToManyField(Post)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user_name
