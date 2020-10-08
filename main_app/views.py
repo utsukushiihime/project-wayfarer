7e16483f7b9a4e548eee0555e677975a067ebf15
BTB0923 <brendan.t.bliss@gmail.com>
2020-10-07 23:12:55 -0400

    added home and base views, installed bootstrap

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+# --- Base Views ---
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+# def about(request):
+#     return render(request, 'about.html')
