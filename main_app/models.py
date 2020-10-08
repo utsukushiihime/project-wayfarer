a946eaf7918d7418b83585270f5b0e1d9243c39e
BTB0923 <brendan.t.bliss@gmail.com>
2020-10-08 13:36:17 -0400

    comments back in views and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,20 +1,20 @@
-# from django.db import models
-# django.contrib.auth.models import User
+from django.db import models
+django.contrib.auth.models import User
 
 # # Create your models here.
 
-# class User_Name(models.Model):
-#     user_name = models.CharField(max_length=150)
-#     user_city = models.CharField(max_length=150)
-#     join_date = models.DateField('Join Date')
-#     post_title = models.CharField(max_length=200)
-#     post = models.TextField(max_length=600)
+class User_Name(models.Model):
+    user_name = models.CharField(max_length=150)
+    user_city = models.CharField(max_length=150)
+    join_date = models.DateField('Join Date')
+    post_title = models.CharField(max_length=200)
+    post = models.TextField(max_length=600)
 
 
 
-# class Image(models.Model):
-#     name= models.CharField(max_length=500)
-#     videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
+class Image(models.Model):
+    name= models.CharField(max_length=500)
+    videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
 
-#     def __str__(self):
-#         return self.name + ": " + str(self.imagefile)
+    def __str__(self):
+        return self.name + ": " + str(self.imagefile)
