dc09220750652351842c987367c299110b34f35f
BTB0923 <brendan.t.bliss@gmail.com>
2020-10-08 13:32:40 -0400

    Adds more landing page bootstrap styling

--- main_app/models.py
+++ main_app/models.py
@@ -3,18 +3,18 @@
 
 # # Create your models here.
 
-class User_Name(models.Model):
-    user_name = models.CharField(max_length=150)
-    user_city = models.CharField(max_length=150)
-    join_date = models.DateField('Join Date')
-    post_title = models.CharField(max_length=200)
-    post = models.TextField(max_length=600)
+# class User_Name(models.Model):
+#     user_name = models.CharField(max_length=150)
+#     user_city = models.CharField(max_length=150)
+#     join_date = models.DateField('Join Date')
+#     post_title = models.CharField(max_length=200)
+#     post = models.TextField(max_length=600)
 
 
 
-class Image(models.Model):
-    name= models.CharField(max_length=500)
-    videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
+# class Image(models.Model):
+#     name= models.CharField(max_length=500)
+#     videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
 
-    def __str__(self):
-        return self.name + ": " + str(self.imagefile)
+#     def __str__(self):
+#         return self.name + ": " + str(self.imagefile)
