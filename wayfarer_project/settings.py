4995ef5d0aec5e52eca24a02b1a4bea9c6a491f0
BTB0923 <brendan.t.bliss@gmail.com>
2020-10-08 16:59:41 -0400

    adds more bootstrap styling, modals to landing page

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
-    # 'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL= "/media/"
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 
 
