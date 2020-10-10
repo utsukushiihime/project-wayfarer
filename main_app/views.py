601f5ccad7ebb4f84831f5cffb428294aa33cc60
creattech <crystalsmcneil@gmail.com>
2020-10-09 21:17:18 -0500

    removed accounts and updated user account in main_app

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-# from .models import Image
-# from .forms import Image_Form
+from .models import Image, User_Profile, City, Post
+from .forms import Image_Form
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -13,31 +16,42 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def user(request):
+    return render(request, 'user/profile.html')
+
 def profile(request):
     return render(request, 'user/profile.html')
 
 def index(request):
     return render(request, 'cities/index.html')
 
+def api(request):
+    return JsonResponse({"status": 200})
 
-# index and create
-
-def post_index(request):
+        
+# View Profile
+@login_required
+def user_index(request):
     if request.method == 'POST':
-        post_form = Cat_Form(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save(commit=False)
-            new_post.user = request.user
-            new_post.save()
-            return redirect()
-
-
-
+        user_form = User_Form(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.user = request.user
+            # save() to the db
+            new_user.save()
+            return redirect('cats_index')
+    users = User_Profile.objects.filter(user=request.user)
+    user_form = User_Form()
+    context = {'user': user, 'user_form': user_form}
+    return render(request, 'user/profile.html', context)
 # --- Image Views ---
 
 def showimage(request):
+    
     lastimage= Image.objects.last()
+
     imagefile= lastimage.imagefile
+
 
     form= Image_Form(request.POST or None, request.FILES or None)
     if form.is_valid():
@@ -46,5 +60,21 @@ def showimage(request):
     context= {'imagefile': imagefile,
               'form': form
               }
-    #set the path for user images      
-    return render(request, 'User/images.html', context)
+    return render(request, 'user/images.html', context)
+
+
+# --- Signup View ---
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('user_index')
+      else:
+        error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +123,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL= "/media/"
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
-LOGIN_REDIRECT_URL = '/'
-#LOGOUT_REDIRECT_URL = "/"
+
+LOGIN_REDIRECT_URL = '/user/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 
 
