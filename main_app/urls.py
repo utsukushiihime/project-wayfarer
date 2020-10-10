c0f3cc671eeb6af409bd4cbcc92379278914850f
creattech <crystalsmcneil@gmail.com>
2020-10-10 18:03:28 -0500

    adds fix to signup form

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('profile/<int:user_id>/detail', views.profile_detail, name='profile_detail'),
+    path('profile/<int:user_id>/', views.profile_detail, name='profile_detail'),
     path('profile/<int:user_id>/edit', views.profile_edit, name='profile_edit'),
     path('cities/', views.cities_index, name="cities_index"),
     path('cities/<int:city_id>/', views.cities_detail, name='cities_detail'), 
     path('posts/<int:post_id>/', views.posts_detail, name='posts_detail'),
-    path('accounts/register', views.signup, name='signup'),
-    path('accounts/login', views.login, name='login'),
+    path('accounts/register/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -64,7 +64,7 @@ def signup(request):
       if form.is_valid():
         user = form.save()
         login(request, user)
-        return redirect('home')
+        return redirect('profile_detail')
       else:
         error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
