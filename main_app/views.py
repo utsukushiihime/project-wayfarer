ce60aea0525bbd591f69e2c9415c27dfc1e27d29
wkriegsman <wkriegsman@gmail.com>
2020-10-11 23:08:36 -0400

    trying to add current city field to sign up page

--- main_app/views.py
+++ main_app/views.py
@@ -72,19 +72,6 @@ def posts_detail(request, post_id):
     return render(request, 'posts/detail.html', context)    
 
 # --- Signup ---
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('home')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'registration/signup.html', {'form': form})
 
 def signup(request):
   error_message = ''
