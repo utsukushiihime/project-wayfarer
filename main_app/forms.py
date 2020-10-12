3b91611ea059ff68803dd56648635516f3e7b657
creattech <crystalsmcneil@gmail.com>
2020-10-12 03:15:31 -0500

    redirects to login after user created

--- main_app/forms.py
+++ main_app/forms.py
@@ -6,11 +6,6 @@ from django import forms
 
 from .models import Profile, City, Post
 
-# class Register_Form(UserCreationForm):
-#     first_name = forms.CharField()
-#     last_name = forms.CharField()
-#     email = forms.EmailField()
-#     #current_city = forms.ModelChoiceField(queryset=City.objects.all())
     
 class Register_Form(UserCreationForm):
     
@@ -22,13 +17,10 @@ class Register_Form(UserCreationForm):
 
     def save(self, commit=True):
         user = super(Register_Form, self).save(commit=False)
-        user.current_city = self.cleaned_data["current_city"]
+        user.current_city = self.cleaned_data["city"]
         if commit:
             user.save()
         return user
-
-
-
         
 class User_Form(ModelForm):
     class Meta:
