0995a3d8c20bb44717cbd3ad5ca12b34d22463e8
creattech <crystalsmcneil@gmail.com>
2020-10-11 19:58:42 -0500

    added styling to post form in forms.py

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import CharField
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -25,11 +26,34 @@ class Profile_Form(ModelForm):
         fields = ['current_city']
 
 class Post_Form(ModelForm):
+
     class Meta:
         model = Post
         fields = [
             'title', 'city','content', 'image', 'user', 'post_date' 
         ]
+        widgets = {
+            'title': forms.TextInput(
+				attrs={
+					'class': 'form-control'
+					}
+				),
+            'content': forms.Textarea(
+				attrs={
+					'class': 'form-control'
+					}
+				),
+            'post_date': forms.TextInput(
+				attrs={
+					'class': 'form-control'
+					}
+				),
+            'image': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+			}
         
 class City_Form(ModelForm):
     class Meta:
